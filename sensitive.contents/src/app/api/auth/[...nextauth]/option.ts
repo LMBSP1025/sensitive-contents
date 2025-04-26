60a295eeed2f43d0f884541ad4c92996b7921f68
LMBSP1025 <100066810+LMBSP1025@users.noreply.github.com>
2025-04-26 20:37:58 +0900

    수정

---
+++ sensitive.contents/src/app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,29 @@
+import { AuthOptions } from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    FacebookProvider({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID || "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          scope: "public_profile,email"
+        }
+      }
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.access_token) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
--- sensitive.contents/src/app/api/auth/[...nextauth]/route.ts
+++ sensitive.contents/src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,5 @@
 import NextAuth from "next-auth/next";
-import { Session, Account, AuthOptions } from "next-auth";
-import InstagramProvider from "next-auth/providers/instagram";
-import { JWT } from "next-auth/jwt";
-
-// 비즈니스 앱용 설정
-export const authOptions: AuthOptions = {
-  providers: [
-    InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID || "",
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET || "",
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }: { token: JWT; account: Account | null }) {
-      // 인증 정보에서 accessToken을 JWT에 저장
-      if (account?.access_token) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      // 타입 단언 추가
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  useSecureCookies: process.env.NODE_ENV === 'production', // 프로덕션에서만 true
-  debug: process.env.NODE_ENV === 'development', // 개발 환경에서 디버그 활성화
-};
+import { authOptions } from "./option";
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
