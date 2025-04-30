afbc018a130bf7c1cffa65741b0595081d564d07
LMBSP1025 <100066810+LMBSP1025@users.noreply.github.com>
2025-05-01 01:23:16 +0900

    오류 수정

--- sensitive.contents/src/app/api/auth/[...nextauth]/route.ts
+++ sensitive.contents/src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, user }: { session: any, user: any }) {
-      if (session.user && user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-};
+import { authOptions } from "./authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- sensitive.contents/src/app/api/comments/route.ts
+++ sensitive.contents/src/app/api/comments/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/authOptions";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
